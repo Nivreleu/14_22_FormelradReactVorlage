8f0563628bd074cb8db6065431aa1064e45a6fec
Minion4242 <timleuenberger718@gmail.com>
2024-06-10 21:12:09 +0200

    Fix for Patch 2

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -2,7 +2,6 @@ import {useState} from "react";
 import '../css/mvp.css';
 import formelrad from "../image/formelradelektronik.gif";
 import InputField from "../formular/InputField";
-
 export default function Formelrad() {
     const [values, setValues] = useState({
         u: 10,
@@ -18,7 +17,7 @@ export default function Formelrad() {
                 </header>
                 <form>
                     <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
-                    <InputField color={"black"} value={values.i} label="Stromstaerke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
+                    <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
                     <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
                     <button type="submit">Calculate</button>
                 </form>
